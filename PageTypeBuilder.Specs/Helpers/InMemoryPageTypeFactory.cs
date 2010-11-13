39cdb40ef233bf45907ca45a6593443510228f2c
joelabrahamsson <mail@joelabrahamsson.com>
2010-11-13 23:42:37 +0100

    Added InMemoryPageTypeFactory

---
+++ PageTypeBuilder.Specs/Helpers/InMemoryPageTypeFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EPiServer.DataAbstraction;
+using PageTypeBuilder.Abstractions;
+
+namespace PageTypeBuilder.Specs.Helpers
+{
+    public class InMemoryPageTypeFactory : IPageTypeFactory
+    {
+        private int nextId;
+        private List<PageType> pageTypes;
+
+        public InMemoryPageTypeFactory()
+        {
+            nextId = 1;
+            pageTypes = new List<PageType>();
+        }
+
+        public PageType Load(string name)
+        {
+            return pageTypes.FirstOrDefault(pageType => pageType.Name == name);
+        }
+
+        public PageType Load(Guid guid)
+        {
+            return pageTypes.FirstOrDefault(pageType => pageType.GUID == guid);
+        }
+
+        public PageType Load(int id)
+        {
+            return pageTypes.FirstOrDefault(pageType => pageType.ID == id);
+        }
+
+        public void Save(PageType pageTypeToSave)
+        {
+            if (pageTypeToSave.IsNew)
+            {
+                pageTypeToSave.ID = nextId;
+                nextId = nextId++;
+                pageTypes.Add(pageTypeToSave);
+            }
+        }
+    }
+}
