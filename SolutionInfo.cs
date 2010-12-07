e167c56a640e4294269738ec6a1b3c540b7dc919
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-07 19:34:13 +0100

    Added conditional AssemblyConfiguration attribute to SolutionInfo

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,15 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("PageTypeBuilder")]
 [assembly: ComVisible(false)]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
