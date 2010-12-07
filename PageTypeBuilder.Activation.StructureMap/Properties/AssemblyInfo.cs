b37bfc7ea507fbed54e5cb8072738dae58243770
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-07 19:26:49 +0100

    Added SolutionInfo.cs and added it as a link to all projects

--- PageTypeBuilder.Activation.StructureMap/Properties/AssemblyInfo.cs
+++ PageTypeBuilder.Activation.StructureMap/Properties/AssemblyInfo.cs
@@ -1,33 +1,3 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PageTypeBuilder.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("PageTypeBuilder.Tests")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1d1e3413-7a8e-4245-8239-c673b5ca9d99")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyTitle("PageTypeBuilder.Activation.StructureMap")]
--- PageTypeBuilder/Properties/AssemblyInfo.cs
+++ PageTypeBuilder/Properties/AssemblyInfo.cs
@@ -1,37 +1,8 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("PageTypeBuilder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Joel Abrahamsson, open source")]
-[assembly: AssemblyProduct("PageTypeBuilder")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fa6c53a4-cbdf-4c29-849e-138be3a69d57")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("PageTypeBuilder.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100134b9a2c2c0dce416e54848eaf631f5a36814bbb34d3a680e78c241cc811e94f39560b0cc7410d0b48842740371c4ee2d9f41f3c349a1f79f4d1aaf42180bb9c0aebf23aff01e827b7da0e889f78005c17ee970feb470baaf130d1f943a1bf5d64b84221cee502220c9e31fb48c363ee997a9051f4908df2f76bd471015c1dac")]
 [assembly: InternalsVisibleTo("PageTypeBuilder.Specs, PublicKey=0024000004800000940000000602000000240000525341310004000001000100819e920dc8394a17bfddcb2f0f9279bbf43d63ae452187b5a14309ccec98bc0bc8c42602d9314987b4b77014747bc82fd76a70163bfbf07159c25e08fa9fbe1bed32627028d0d22773b8e300d5d73dd70e4bf23ac168ba65b22f7eb5d5716cbea0d735a70907977a13f95b1d57a459b1f7a87d427ec048cd402cd37fd4cce5e6")]
-[assembly: CLSCompliant(true)]
