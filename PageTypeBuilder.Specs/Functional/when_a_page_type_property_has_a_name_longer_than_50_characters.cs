2d2ed55bdd40dc89b7456a7a9733634589fb25a9
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-12 18:31:25 +0100

    Added IntegerExtensions to specs project and a new spec

---
+++ PageTypeBuilder.Specs/Functional/when_a_page_type_property_has_a_name_longer_than_50_characters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Machine.Specifications;
+using PageTypeBuilder.Specs.Helpers;
+
+namespace PageTypeBuilder.Specs
+{
+    public class when_a_page_type_property_has_a_name_longer_than_50_characters
+    {
+        static InMemoryContext environmentContext = new InMemoryContext();
+        static Exception thrownException;
+
+        Establish context = () => 
+            environmentContext.AddPageTypeClass(type =>
+                type.Name = 51.CharactersLongAlphanumericString());
+
+        Because of =
+            () => thrownException = Catch.Exception(() => environmentContext.PageTypeSynchronizer.SynchronizePageTypes());
+
+        It should_throw_a_PageTypeBuilderException =
+            () => thrownException.ShouldBeOfType<PageTypeBuilderException>();
+    }
+}
---
+++ PageTypeBuilder.Specs/Helpers/IntegerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace PageTypeBuilder.Specs.Helpers
+{
+    public static class IntegerExtensions
+    {
+        private static string alphanumericChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        public static string CharactersLongAlphanumericString(this int number)
+        {
+            var random = new Random();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < number; i++)
+                result.Append(alphanumericChars[(int)(random.NextDouble() * alphanumericChars.Length)]);
+
+            return result.ToString();
+        }
+    }
+}
