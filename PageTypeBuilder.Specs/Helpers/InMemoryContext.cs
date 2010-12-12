9038b2c7618b30de38639825d3d13dce8e1299e6
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-12 18:06:18 +0100

    Added InMemoryContext which is used by specs

---
+++ PageTypeBuilder.Specs/Helpers/InMemoryContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection.Emit;
+using PageTypeBuilder.Abstractions;
+using PageTypeBuilder.Reflection;
+using PageTypeBuilder.Synchronization;
+using StructureMap;
+
+namespace PageTypeBuilder.Specs.Helpers
+{
+    public class InMemoryContext
+    {
+        public InMemoryContext()
+        {
+            Container = new Container(new InMemoryComponentsRegistry());
+        }
+
+        public Container Container { get; set; }
+
+        public InMemoryAssemblyLocator AssemblyLocator
+        {
+            get
+            {
+                return (InMemoryAssemblyLocator)Container.GetInstance<IAssemblyLocator>();
+            }
+        }
+
+        public InMemoryPageTypeFactory PageTypeFactory
+        {
+            get
+            {
+                return (InMemoryPageTypeFactory) Container.GetInstance<IPageTypeFactory>();
+            }
+        }
+
+        public InMemoryPageDefinitionFactory PageDefinitionFactory
+        {
+            get
+            {
+                return (InMemoryPageDefinitionFactory) Container.GetInstance<IPageDefinitionFactory>();
+            }
+        }
+
+        public PageTypeSynchronizer PageTypeSynchronizer
+        {
+            get
+            {
+                return Container.GetInstance<PageTypeSynchronizer>();
+            }
+        }
+
+        public void AddTypeInheritingFromTypedPageData(Action<TypeSpecification> typeSpecificationExpression)
+        {
+            TypeBuilder type = FunctionalSpecFixture.CreateTypeInheritingFromTypedPageData(typeSpecificationExpression);
+
+            AssemblyLocator.Add(type.Assembly);
+        }
+
+        public void AddTypeInheritingFromTypedPageData(ModuleBuilder module, Action<TypeSpecification> typeSpecificationExpression)
+        {
+            TypeBuilder type = FunctionalSpecFixture.CreateTypeInheritingFromTypedPageData(module, typeSpecificationExpression);
+
+            AssemblyLocator.Add(type.Assembly);
+        }
+    }
+}
