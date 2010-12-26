1c3c88166e73d997230e1e070c6f249d8fa8b483
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-26 13:04:53 +0100

    Added Its to when_two_page_type_classes_have_same_guid spec with Ignore attributes for later implementation

--- PageTypeBuilder.Specs/Synchronization/when_two_page_type_classes_have_same_guid.cs
+++ PageTypeBuilder.Specs/Synchronization/when_two_page_type_classes_have_same_guid.cs
@@ -4,25 +4,26 @@ using PageTypeBuilder.Specs.Helpers.TypeBuildingDsl;
 
 namespace PageTypeBuilder.Specs.Synchronization
 {
-    [Ignore]
     [Subject("Synchronization")]
     public class when_two_page_type_classes_have_same_guid
         : SynchronizationSpecs
     {
         static Exception thrownException;
+        static string firstPageTypeName = "aPageType";
+        static string secondPageTypeName = "anotherPageType";
 
         Establish context = () =>
         {
             string guid = new Guid().ToString();
             SyncContext.AddTypeInheritingFromTypedPageData(type =>
             {
-                type.Name = "aPageType";
+                type.Name = firstPageTypeName;
                 type.Attributes.Add(CreatePageTypeAttributeSpecification(guid));
             });
 
             SyncContext.AddTypeInheritingFromTypedPageData(type =>
             {
-                type.Name = "anotherPageType";
+                type.Name = secondPageTypeName;
                 type.Attributes.Add(CreatePageTypeAttributeSpecification(guid));
             });
         };
@@ -40,7 +41,19 @@ namespace PageTypeBuilder.Specs.Synchronization
             thrownException = Catch.Exception(
                 () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes());
 
+        It should_throw_an_Exception = () =>
+            thrownException.ShouldNotBeNull();
+
+        [Ignore]
         It should_throw_a_PageTypeBuilderException = () =>
             thrownException.ShouldBeOfType<PageTypeBuilderException>();
+
+        [Ignore] 
+        It should_throw_Exception_containing_the_first_page_types_name = () =>
+            thrownException.Message.ShouldContain(secondPageTypeName);
+
+        [Ignore]
+        It should_throw_Exception_containing_the_second_page_types_name = () =>
+            thrownException.Message.ShouldContain(firstPageTypeName);
     }
 }
