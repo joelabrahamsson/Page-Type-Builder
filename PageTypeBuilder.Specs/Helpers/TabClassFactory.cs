9460aa5b1589b72c3eaf9ec6122ae8296a425d56
joelabrahamsson <mail@joelabrahamsson.com>
2010-12-27 13:15:24 +0100

    Added helper class for creating Tab classes for specs

---
+++ PageTypeBuilder.Specs/Helpers/TabClassFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using EPiServer.Security;
+using PageTypeBuilder.Specs.Helpers.TypeBuildingDsl;
+
+namespace PageTypeBuilder.Specs.Helpers
+{
+    public class TabClassFactory
+    {
+        public static Type CreateTabClass(string className, string tabName, AccessLevel requiredAccess, int sortIndex)
+        {
+            var moduleBuilder = ReflectionExtensions.CreateModuleWithReferenceToPageTypeBuilder("AssemblyWithTab");
+
+            var nameProperty = new PropertySpecification();
+            nameProperty.Name = "Name";
+            nameProperty.Type = typeof(string);
+            nameProperty.GetterImplementation = (typeBuilder) =>
+            {
+                MethodBuilder getMethodBuilder =
+                    typeBuilder.DefineMethod("get_Name", MethodAttributes.Public | MethodAttributes.Virtual, typeof(string), Type.EmptyTypes);
+
+                ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
+                ilGenerator.Emit(OpCodes.Ldstr, tabName);
+                ilGenerator.Emit(OpCodes.Ret);
+                return getMethodBuilder;
+            };
+
+            var requiredAccessProperty = new PropertySpecification();
+            requiredAccessProperty.Name = "RequiredAccess";
+            requiredAccessProperty.Type = typeof(AccessLevel);
+            requiredAccessProperty.GetterImplementation = (typeBuilder) =>
+            {
+                MethodBuilder getMethodBuilder =
+                    typeBuilder.DefineMethod("get_RequiredAccess", MethodAttributes.Public | MethodAttributes.Virtual, typeof(AccessLevel), Type.EmptyTypes);
+
+                ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
+                ilGenerator.DeclareLocal(typeof(AccessLevel));
+                ilGenerator.Emit(OpCodes.Ldc_I4, (int)requiredAccess);
+                ilGenerator.Emit(OpCodes.Stloc_0);
+                ilGenerator.Emit(OpCodes.Ldloc_0);
+                ilGenerator.Emit(OpCodes.Ret);
+                return getMethodBuilder;
+            };
+
+            var sortIndexProperty = new PropertySpecification();
+            sortIndexProperty.Name = "SortIndex";
+            sortIndexProperty.Type = typeof(int);
+            sortIndexProperty.GetterImplementation = (typeBuilder) =>
+            {
+                MethodBuilder getMethodBuilder =
+                    typeBuilder.DefineMethod("get_SortIndex", MethodAttributes.Public | MethodAttributes.Virtual, typeof(int), Type.EmptyTypes);
+
+                ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
+                ilGenerator.DeclareLocal(typeof(int));
+                ilGenerator.Emit(OpCodes.Ldc_I4, sortIndex);
+                ilGenerator.Emit(OpCodes.Stloc_0);
+                ilGenerator.Emit(OpCodes.Ldloc_0);
+                ilGenerator.Emit(OpCodes.Ret);
+                return getMethodBuilder;
+            };
+
+            return moduleBuilder.CreateClass(type =>
+            {
+                type.Name = "NameOfTheTabClass";
+                type.ParentType = typeof(Tab);
+                type.Properties.Add(nameProperty);
+                type.Properties.Add(requiredAccessProperty);
+                type.Properties.Add(sortIndexProperty);
+            });
+        }
+    }
+}
--- PageTypeBuilder.Specs/Synchronization/TabSynchronization/when_a_new_class_inheriting_from_Tab_has_been_added.cs
+++ PageTypeBuilder.Specs/Synchronization/TabSynchronization/when_a_new_class_inheriting_from_Tab_has_been_added.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using EPiServer.Security;
+﻿using EPiServer.Security;
 using Machine.Specifications;
-using PageTypeBuilder.Specs.Helpers.TypeBuildingDsl;
+using PageTypeBuilder.Specs.Helpers;
 
 namespace PageTypeBuilder.Specs.Synchronization.TabSynchronization
 {
@@ -11,76 +8,15 @@ namespace PageTypeBuilder.Specs.Synchronization.TabSynchronization
     public class when_a_new_class_inheriting_from_Tab_has_been_added
         : SynchronizationSpecs
     {
-        static string className = "NameOfTheTabClass";
         static string tabName = "ValueOfNameProperty";
         static AccessLevel requiredAccess = AccessLevel.Publish;
         static int sortIndex = 12;
 
-        Establish context = () =>
-        {
-            var moduleBuilder = ReflectionExtensions.CreateModuleWithReferenceToPageTypeBuilder("AssemblyWithTab");
-
-            var nameProperty = new PropertySpecification();
-            nameProperty.Name = "Name";
-            nameProperty.Type = typeof (string);
-            nameProperty.GetterImplementation = (typeBuilder) =>
-                {
-                    MethodBuilder getMethodBuilder =
-                        typeBuilder.DefineMethod("get_Name", MethodAttributes.Public | MethodAttributes.Virtual, typeof(string), Type.EmptyTypes);
-
-                    typeBuilder.DefineMethodOverride(getMethodBuilder, typeof(Tab).GetMethod("get_Name"));
-
-                    ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
-                    ilGenerator.Emit(OpCodes.Ldstr, tabName);
-                    ilGenerator.Emit(OpCodes.Ret);
-                    return getMethodBuilder;
-                };
-
-            var requiredAccessProperty = new PropertySpecification();
-            requiredAccessProperty.Name = "RequiredAccess";
-            requiredAccessProperty.Type = typeof(AccessLevel);
-            requiredAccessProperty.GetterImplementation = (typeBuilder) =>
-            {
-                MethodBuilder getMethodBuilder =
-                    typeBuilder.DefineMethod("get_RequiredAccess", MethodAttributes.Public | MethodAttributes.Virtual, typeof(AccessLevel), Type.EmptyTypes);
-
-                typeBuilder.DefineMethodOverride(getMethodBuilder, typeof(Tab).GetMethod("get_RequiredAccess"));
-                ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
-                ilGenerator.DeclareLocal(typeof(AccessLevel));
-                ilGenerator.Emit(OpCodes.Ldc_I4, (int) requiredAccess);
-                ilGenerator.Emit(OpCodes.Stloc_0);
-                ilGenerator.Emit(OpCodes.Ldloc_0);
-                ilGenerator.Emit(OpCodes.Ret);
-                return getMethodBuilder;
-            };
-
-            var sortIndexProperty = new PropertySpecification();
-            sortIndexProperty.Name = "SortIndex";
-            sortIndexProperty.Type = typeof(int);
-            sortIndexProperty.GetterImplementation = (typeBuilder) =>
-            {
-                MethodBuilder getMethodBuilder =
-                    typeBuilder.DefineMethod("get_SortIndex", MethodAttributes.Public | MethodAttributes.Virtual, typeof(int), Type.EmptyTypes);
-
-                typeBuilder.DefineMethodOverride(getMethodBuilder, typeof(Tab).GetMethod("get_SortIndex"));
-                ILGenerator ilGenerator = getMethodBuilder.GetILGenerator();
-                ilGenerator.DeclareLocal(typeof(int));
-                ilGenerator.Emit(OpCodes.Ldc_I4, sortIndex);
-                ilGenerator.Emit(OpCodes.Stloc_0);
-                ilGenerator.Emit(OpCodes.Ldloc_0);
-                ilGenerator.Emit(OpCodes.Ret);
-                return getMethodBuilder;
-            };
-            
-            moduleBuilder.CreateClass(type =>
-                {
-                    type.Name = className;
-                    type.ParentType = typeof(Tab);
-                    type.Properties.Add(nameProperty);
-                    type.Properties.Add(requiredAccessProperty);
-                    type.Properties.Add(sortIndexProperty);
-                });
-        };
+        Establish context = () => TabClassFactory.CreateTabClass(
+            "NameOfClass",
+            tabName, 
+            requiredAccess, 
+            sortIndex);
 
         Because of =
             () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
