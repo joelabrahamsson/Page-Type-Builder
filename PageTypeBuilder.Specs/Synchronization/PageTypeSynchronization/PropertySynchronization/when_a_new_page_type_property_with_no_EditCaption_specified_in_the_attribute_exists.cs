2978cf537fa3a0aef10449cbe8db0c395b9d05fc
joelabrahamsson <mail@joelabrahamsson.com>
2011-01-06 22:01:02 +0100

    Added a spec for what happens when a page type property doesn't have an EditCaption specified

---
+++ PageTypeBuilder.Specs/Synchronization/PageTypeSynchronization/PropertySynchronization/when_a_new_page_type_property_with_no_EditCaption_specified_in_the_attribute_exists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using PageTypeBuilder.Specs.Helpers.TypeBuildingDsl;
+
+namespace PageTypeBuilder.Specs.Synchronization.PageTypeSynchronization.PropertySynchronization
+{
+    [Subject("Synchronization")]
+    public class when_a_new_page_type_property_with_no_EditCaption_specified_in_the_attribute_exists
+        : SynchronizationSpecs
+    {
+        static string className = "MyPageTypeClass";
+        static string propertyName = "NameOfTheProperty";
+
+        Establish context = () => SyncContext.AddPageTypeClassToAppDomain(type => 
+                type.AddProperty(property =>
+                {
+                    property.Name = propertyName;
+                    property.Type = typeof (string);
+                    property.AddAttributeTemplate(new PageTypePropertyAttribute());
+                })
+            );
+
+        Because of =
+            () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_create_a_new_PageDefinition_with_an_EditCaption_equal_to_the_propertys_name =
+            () => SyncContext.PageDefinitionFactory.List().First()
+                .EditCaption.ShouldEqual(propertyName);
+    }
+}
