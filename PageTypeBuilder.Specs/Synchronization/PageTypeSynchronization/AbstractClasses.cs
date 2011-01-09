3e583cc3c5ad0a13d1572ca968fde0d50d630526
joelabrahamsson <mail@joelabrahamsson.com>
2011-01-09 02:14:36 +0100

    Grouped page type synchronization specs in files whose names describes the nature of what they are specifying

---
+++ PageTypeBuilder.Specs/Synchronization/PageTypeSynchronization/AbstractClasses.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Machine.Specifications;
+using PageTypeBuilder.Specs.Helpers;
+using PageTypeBuilder.Specs.Helpers.TypeBuildingDsl;
+
+namespace PageTypeBuilder.Specs.Synchronization.PageTypeSynchronization.AbstractClasses
+{
+    [Subject("Synchronization")]
+    public class when_a_new_abstract_page_type_class_has_been_added
+        : SynchronizationSpecs
+    {
+        static string className = "MyPageTypeClass";
+        static int numberOfPageTypesBeforeSynchronization;
+
+        Establish context = () =>
+        {
+            numberOfPageTypesBeforeSynchronization =
+                SyncContext.PageTypeFactory.List().Count();
+
+            SyncContext.AddTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = className;
+                type.TypeAttributes = TypeAttributes.Abstract;
+                type.AddAttributeTemplate(new PageTypeAttribute());
+            });
+        };
+
+        Because of =
+            () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_not_create_a_new_page_type =
+            () => SyncContext.PageTypeFactory.List().Count()
+                .ShouldEqual(numberOfPageTypesBeforeSynchronization);
+    }
+
+    [Subject("Synchronization")]
+    public class when_a_class_inheriting_from_an_abstract_page_type_class_in_another_assembly_with_no_name_specified_in_the_attribute_has_been_added
+        : SynchronizationSpecs
+    {
+        static string className = "MyPageTypeClass";
+
+        Establish context = () =>
+        {
+            Type abstractClass = PageTypeClassFactory.CreateTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = "BaseClass";
+                type.TypeAttributes = TypeAttributes.Abstract | TypeAttributes.Public;
+                type.AddAttributeTemplate(new PageTypeAttribute());
+            });
+            SyncContext.AssemblyLocator.Add(abstractClass.Assembly);
+
+            Type pageTypeClass = PageTypeClassFactory.CreateTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = className;
+                type.ParentType = abstractClass;
+            });
+            SyncContext.AssemblyLocator.Add(pageTypeClass.Assembly);
+        };
+
+        Because of =
+            () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_create_a_new_page_type_with_the_name_of_the_concrete_class =
+            () => SyncContext.PageTypeFactory.Load(className).ShouldNotBeNull();
+    }
+
+    [Subject("Synchronization")]
+    public class when_a_class_inheriting_from_an_abstract_page_type_class_in_the_same_assembly_with_no_name_specified_has_been_added
+        : SynchronizationSpecs
+    {
+        static string className = "MyPageTypeClass";
+
+        Establish context = () =>
+        {
+            Type abstractClass = PageTypeClassFactory.CreateTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = "BaseClass";
+                type.TypeAttributes = TypeAttributes.Abstract;
+                type.AddAttributeTemplate(new PageTypeAttribute());
+            });
+
+            SyncContext.AssemblyLocator.Add(abstractClass.Assembly);
+
+            ((ModuleBuilder)abstractClass.Module).CreateClass(type =>
+            {
+                type.Name = className;
+                type.ParentType = abstractClass;
+            });
+        };
+
+        Because of =
+            () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_create_a_new_page_type_with_the_name_of_the_concrete_class =
+            () => SyncContext.PageTypeFactory.Load(className).ShouldNotBeNull();
+    }
+}
---
+++ PageTypeBuilder.Specs/Synchronization/PageTypeSynchronization/NoSaving.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using PageTypeBuilder.Abstractions;
+using PageTypeBuilder.Specs.Helpers;
+using PageTypeBuilder.Synchronization;
+
+namespace PageTypeBuilder.Specs.Synchronization.PageTypeSynchronization.NoSaving
+{
+    [Subject("Synchronization")]
+    public class when_a_page_type_class_with_everything_specified_in_the_attribute_matches_an_existing_page_type_and_the_values_in_the_attribute_is_the_same_as_corresponding_values_in_the_PageType
+        : SynchronizationSpecs
+    {
+        static string pageTypeName = "NameOfThePageType";
+        static int pageTypeId;
+
+        Establish context = () =>
+        {
+            var pageTypeAttribute = AttributeHelper.CreatePageTypeAttributeWithEverythingSpeficied(SyncContext);
+            var existingPageType = PageTypeMother.CreatePageTypeWithSameValuesAsAttribute(SyncContext, pageTypeAttribute);
+            existingPageType = SyncContext.PageTypeFactory.CreateNew();
+            SyncContext.PageTypeFactory.Save(existingPageType);
+            pageTypeId = existingPageType.ID;
+            SyncContext.PageTypeFactory.ResetNumberOfSaves();
+
+            var attributeSpecification =
+                AttributeHelper.CreatePageTypeAttributeSpecification(pageTypeAttribute.Guid.ToString());
+            attributeSpecification.Template = pageTypeAttribute;
+
+            SyncContext.AddTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = pageTypeName;
+                type.Attributes.Add(attributeSpecification);
+            });
+        };
+
+        Because of = () =>
+            SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_not_save_the_PageType = () =>
+            SyncContext.PageTypeFactory.GetNumberOfSaves(pageTypeId).ShouldEqual(0);
+    }
+
+    [Subject("Synchronization")]
+    public class when_page_type_updation_has_been_disabled_through_configuration_and_a_new_page_type_class_exists
+        : SynchronizationSpecs
+    {
+        static int numberOfPageTypesBeforeSynchronization;
+
+        Establish context = () =>
+        {
+            numberOfPageTypesBeforeSynchronization =
+                    SyncContext.PageTypeFactory.List().Count();
+
+            SyncContext.CreateAndAddPageTypeClassToAppDomain(type =>
+            {
+                type.Name = "NameOfThePageTypeClass";
+            });
+
+            SyncContext.Configuration.SetDisablePageTypeUpdation(true);
+        };
+
+        Because of =
+            () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_not_create_a_new_page_type =
+            () => SyncContext.PageTypeFactory.List().Count()
+                .ShouldEqual(numberOfPageTypesBeforeSynchronization);
+    }
+
+    [Subject("Synchronization")]
+    public class when_page_type_updation_has_been_disabled_through_configuration_and_a_page_type_class_has_the_same_name_as_an_existing_PageType_but_a_different_SortOrder
+        : SynchronizationSpecs
+    {
+        static int pageTypeId;
+        static int originalSortOrder;
+
+        Establish context = () =>
+        {
+            IPageType existingPageType = SyncContext.PageTypeFactory.CreateNew();
+            existingPageType.Name = "NameOfThePageType";
+            existingPageType.FileName = PageTypeUpdater.DefaultPageTypeFilename;
+            existingPageType.SortOrder = originalSortOrder;
+
+            SyncContext.PageTypeFactory.Save(existingPageType);
+            pageTypeId = existingPageType.ID;
+            SyncContext.PageTypeFactory.ResetNumberOfSaves();
+
+            var attribute = new PageTypeAttribute();
+            attribute.SortOrder = 2;
+            SyncContext.AddTypeInheritingFromTypedPageData(type =>
+            {
+                type.Name = existingPageType.Name;
+                type.AddAttributeTemplate(attribute);
+            });
+
+            SyncContext.Configuration.SetDisablePageTypeUpdation(true);
+        };
+
+        Because of = () =>
+            SyncContext.PageTypeSynchronizer.SynchronizePageTypes();
+
+        It should_not_update_the_existing_page_types_SortOrder = () =>
+            SyncContext.PageTypeFactory.Load(pageTypeId).SortOrder.ShouldEqual(originalSortOrder);
+
+        It should_not_save_the_PageType = () =>
+            SyncContext.PageTypeFactory.GetNumberOfSaves(pageTypeId).ShouldEqual(0);
+    }
+}
