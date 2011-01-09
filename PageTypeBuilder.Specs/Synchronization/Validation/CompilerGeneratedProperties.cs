07fd2bbdedbc33e2b6dfa2ed650de008db372466
joelabrahamsson <mail@joelabrahamsson.com>
2011-01-09 19:19:08 +0100

    Added spec for validation that compiler generated page type properties don't have private setters

--- PageTypeBuilder.Specs/Synchronization/Validation/CompilerGeneratedProperties.cs
+++ PageTypeBuilder.Specs/Synchronization/Validation/CompilerGeneratedProperties.cs
@@ -73,4 +73,42 @@ namespace PageTypeBuilder.Specs.Synchronization.Validation
         It should_not_throw_an_Exception = () =>
             thrownException.ShouldBeNull();
     }
+
+    public class when_a_property_in_a_page_type_class_has_a_PageTypePropertyAttribute_and_is_compiler_generated_with_a_private_setter
+        : SynchronizationSpecs
+    {
+        static Exception thrownException;
+        static string pageTypeName = "NameOfThePropertysPageType";
+        static string propertyName = "ThePropertysName";
+
+        Establish context = () => SyncContext.CreateAndAddPageTypeClassToAppDomain(type =>
+        {
+            type.Name = pageTypeName;
+            type.AddProperty(property =>
+            {
+                property.Name = propertyName;
+                property.AddAttributeTemplate(new PageTypePropertyAttribute());
+                property.Type = typeof(string);
+                property.AnnotateAsCompilerGenerated = true;
+                property.GetterAttributes = MethodAttributes.Public | MethodAttributes.Virtual;
+                property.SetterAttributes = MethodAttributes.Private | MethodAttributes.Virtual;
+            });
+        });
+
+        Because of = () =>
+            thrownException = Catch.Exception(
+                () => SyncContext.PageTypeSynchronizer.SynchronizePageTypes());
+
+        It should_throw_an_Exception = () =>
+            thrownException.ShouldNotBeNull();
+
+        It should_throw_a_PageTypeBuilderException = () =>
+            thrownException.ShouldBeOfType<PageTypeBuilderException>();
+
+        It should_throw_an_Exception_whose_Message_should_contain_the_propertys_name = () =>
+            thrownException.Message.ShouldContain(propertyName);
+
+        It should_throw_an_Exception_whose_Message_should_contain_the_page_type_class_name = () =>
+            thrownException.Message.ShouldContain(pageTypeName);
+    }
 }
