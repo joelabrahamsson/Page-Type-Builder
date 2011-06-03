71fce706ede3d6f81c92e71df8b3ad77b64e82a0
joelabrahamsson <mail@joelabrahamsson.com>
2011-06-03 23:29:22 +0200

    Removed unnecessary code from AbstractGlobalPropertySettings.cs

--- PageTypeBuilder.Specs/ExampleImplementations/AbstractGlobalPropertySettings.cs
+++ PageTypeBuilder.Specs/ExampleImplementations/AbstractGlobalPropertySettings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using EPiServer.Editor.TinyMCE;
+﻿using EPiServer.Editor.TinyMCE;
 
 namespace PageTypeBuilder.Specs.ExampleImplementations
 {
@@ -10,18 +6,9 @@ namespace PageTypeBuilder.Specs.ExampleImplementations
     {
         public void UpdateSettings(TinyMCESettings settings)
         {
-            settings.Width = int.MaxValue;
         }
 
-        public static bool MatchesUpdatedSettings(TinyMCESettings settings)
-        {
-            return settings.Width == int.MaxValue;
-        }
-
-        public int GetSettingsHashCode(TinyMCESettings tinyMceSettings)
-        {
-            return tinyMceSettings.Width;
-        }
+        public abstract int GetSettingsHashCode(TinyMCESettings tinyMceSettings);
 
         public virtual bool OverWriteExistingSettings
         {
@@ -40,48 +27,6 @@ namespace PageTypeBuilder.Specs.ExampleImplementations
 
         public virtual bool? IsDefault { get; set; }
 
-
-        public bool Match(EPiServer.Core.PropertySettings.PropertySettingsWrapper propertySettingsWrapper)
-        {
-            return DisplayName.Equals(propertySettingsWrapper.DisplayName);
-        }
-
-        public static bool HasDefaultValuesExceptUpdated(TinyMCESettings settings)
-        {
-            var defaultValues = (TinyMCESettings)new TinyMCESettings().GetDefaultValues();
-
-            return Serialize(settings).Equals(Serialize(defaultValues));
-        }
-
-        public static string Serialize(TinyMCESettings settings)
-        {
-            var buffer = new StringBuilder();
-            buffer.Append(settings.ContentCss);
-            AppendDelimiter(buffer);
-            buffer.Append(settings.Height);
-            AppendDelimiter(buffer);
-            buffer.Append("NonVisualPlugins");
-            foreach (var nonVisualPlugin in settings.NonVisualPlugins)
-            {
-                buffer.Append(nonVisualPlugin);
-                AppendDelimiter(buffer);
-            }
-            buffer.Append("ToolbarRows");
-            foreach (var toolbarRow in settings.ToolbarRows)
-            {
-                buffer.Append("Row");
-                foreach (var button in toolbarRow.Buttons)
-                {
-                    buffer.Append(button);
-                    AppendDelimiter(buffer);
-                }
-            }
-            return buffer.ToString();
-        }
-
-        private static void AppendDelimiter(StringBuilder buffer)
-        {
-            buffer.Append("|");
-        }
+        public abstract bool Match(EPiServer.Core.PropertySettings.PropertySettingsWrapper propertySettingsWrapper);
     }
 }
