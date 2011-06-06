24f1459e673edb0976fa2df1dd29313eed0b2199
Stefan Forsberg <cykelkatalogen@popkram.com>
2011-06-06 09:25:15 +0200

    Added iniialization module for structure map activator

---
+++ PageTypeBuilder.Activation.StructureMap/content/PageTypeBuilderInitializer.cs.pp
@@ -0,0 +1,26 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using PageTypeBuilder;
+using PageTypeBuilder.Activation.StructureMap;
+using SE1177.Core.Framework.IOC;
+
+namespace $rootnamespace$
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(Initializer))]
+    public class PageTypeBuilderInitializer : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            PageTypeResolver.Instance.Activator = new StructureMapTypedPageActivator(IOC.Container);
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
