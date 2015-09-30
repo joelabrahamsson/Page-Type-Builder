6a5d802c5ceba25dddb3d638888986effd208019
Ruslan Minasian <rminasian@outlook.com>
2015-09-30 15:11:34 +0300

    Reset page type cache when receiving remote event from another server

--- PageTypeBuilder/Initializer.cs
+++ PageTypeBuilder/Initializer.cs
@@ -1,6 +1,10 @@
-﻿using Autofac;
+﻿using System.Web.Caching;
+
+using Autofac;
 using EPiServer;
 using EPiServer.Core.PropertySettings;
+using EPiServer.Events;
+using EPiServer.Events.Clients;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
 using PageTypeBuilder.Abstractions;
@@ -9,26 +13,29 @@ using PageTypeBuilder.Discovery;
 using PageTypeBuilder.Reflection;
 using PageTypeBuilder.Synchronization;
 using PageTypeBuilder.Synchronization.Validation;
-using InitializationModule=EPiServer.Web.InitializationModule;
+using InitializationModule = EPiServer.Web.InitializationModule;
 
 namespace PageTypeBuilder
 {
     [ModuleDependency(typeof(InitializationModule))]
-    public class Initializer : IInitializableModule 
+    public class Initializer : IInitializableModule
     {
+        private IContainer container;
+
         public void Initialize(InitializationEngine context)
         {
             var containerBuilder = new ContainerBuilder();
             var defaultBootstrapper = new DefaultBootstrapper();
             defaultBootstrapper.Configure(containerBuilder);
-            var container = containerBuilder.Build();
-            
+            container = containerBuilder.Build();
+
             PageTypeSynchronizer synchronizer = container.Resolve<PageTypeSynchronizer>();
             synchronizer.SynchronizePageTypes();
 
             DataFactory.Instance.LoadedPage += DataFactory_LoadedPage;
             DataFactory.Instance.LoadedChildren += DataFactory_LoadedChildren;
             DataFactory.Instance.LoadedDefaultPageData += DataFactory_LoadedPage;
+            Event.Get(CacheManager.RemoveFromCacheEventId).Raised += CacheManager_ItemRemoved;
         }
 
         public void Preload(string[] parameters)
@@ -41,11 +48,12 @@ namespace PageTypeBuilder
             DataFactory.Instance.LoadedPage -= DataFactory_LoadedPage;
             DataFactory.Instance.LoadedChildren -= DataFactory_LoadedChildren;
             DataFactory.Instance.LoadedDefaultPageData -= DataFactory_LoadedPage;
+            Event.Get(CacheManager.RemoveFromCacheEventId).Raised -= CacheManager_ItemRemoved;
         }
 
         static void DataFactory_LoadedPage(object sender, PageEventArgs e)
         {
-            if(e.Page == null)
+            if (e.Page == null)
                 return;
 
             e.Page = PageTypeResolver.Instance.ConvertToTyped(e.Page);
@@ -56,6 +64,16 @@ namespace PageTypeBuilder
             for (int i = 0; i < e.Children.Count; i++)
             {
                 e.Children[i] = PageTypeResolver.Instance.ConvertToTyped(e.Children[i]);
+            }
+        }
+
+        void CacheManager_ItemRemoved(object sender, EventNotificationEventArgs e)
+        {
+            var key = e.Param as string;
+            if (key == "EP:PageType" && e.RaiserId != CacheManager.LocalCacheManagerRaiserId && container != null)
+            {
+                var synchronizer = container.Resolve<PageTypeSynchronizer>();
+                synchronizer.SynchronizePageTypes(true);
             }
         }
 
